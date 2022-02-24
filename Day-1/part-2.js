a828e94886e8a260bd29fe6d4e0a434c2fd0f238
sigristarisa <arisasigrist.ch@gmail.com>
2022-02-24 18:04:12 +0100

    has the answer to part 2

---
+++ Day-1/part-2.js
@@ -0,0 +1,16 @@
+const input = require("./input");
+
+const answer = () => {
+  let count = 0;
+  for (let i = 0; i < input.arr.length - 1; i++) {
+    if (
+      input.arr[i] + input.arr[i + 1] + input.arr[i + 2] <
+      input.arr[i + 1] + input.arr[i + 2] + input.arr[i + 3]
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(answer());
