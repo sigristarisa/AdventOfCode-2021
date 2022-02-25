1f69e828f1250f61ddd2c8ce4d2addc1e9b945e5
sigristarisa <arisasigrist.ch@gmail.com>
2022-02-25 10:23:46 +0100

    cleaned the code or Day-1

--- Day-1/part-1.js
+++ Day-1/part-1.js
@@ -2,7 +2,7 @@ const input = require("./input");
 
 const answer = () => {
   let count = 0;
-  for (let i = 0; i < input.arr.length - 1; i++) {
+  for (let i = 0; i <= input.arr.length - 1; i++) {
     if (input.arr[i] < input.arr[i + 1]) {
       count++;
     }
