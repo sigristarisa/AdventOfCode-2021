49bc6353627d26ba5b9f78458a43f1f5b86f1348
sigristarisa <arisasigrist.ch@gmail.com>
2022-02-25 12:28:22 +0100

    has a cleaner code of Day-3 part-1

--- Day-3/part-1.js
+++ Day-3/part-1.js
@@ -1,8 +1,9 @@
 const input = require("./input");
 
-const checkGamma = () => {
+const answer = () => {
   const gamma = [];
-  for (let i = 0; i <= 12; i++) {
+  const epsilon = [];
+  for (let i = 0; i <= input.arr[0].length; i++) {
     let zero = 0;
     let one = 0;
     for (let j = 0; j <= input.arr.length - 1; j++) {
@@ -14,32 +15,13 @@ const checkGamma = () => {
     }
     if (zero < one) {
       gamma.push("1");
-    } else if (zero > one) {
-      gamma.push("0");
-    }
-  }
-  return parseInt(gamma.join(""), 2);
-};
-
-const checkEpsilon = () => {
-  const epsilon = [];
-  for (let i = 0; i <= 12; i++) {
-    let zero = 0;
-    let one = 0;
-    for (let j = 0; j <= input.arr.length - 1; j++) {
-      if (input.arr[j][i] === "0") {
-        zero++;
-      } else if (input.arr[j][i] === "1") {
-        one++;
-      }
-    }
-    if (zero < one) {
       epsilon.push("0");
     } else if (zero > one) {
+      gamma.push("0");
       epsilon.push("1");
     }
   }
-  return parseInt(epsilon.join(""), 2);
+  return parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
 };
 
-console.log(checkEpsilon() * checkGamma());
+console.log(answer());
