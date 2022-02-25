db31a379c43513cb1ddcb8eedca72318161c6848
sigristarisa <arisasigrist.ch@gmail.com>
2022-02-25 09:13:15 +0100

    has the answer of part 1

---
+++ Day-2/part-1.js
@@ -0,0 +1,19 @@
+const input = require("./input");
+
+const answer = () => {
+  let horizontal = 0;
+  let depth = 0;
+  for (let i = 0; i < input.arr.length; i++) {
+    let command = input.arr[i].split(" ");
+    if (command[0] === "forward") {
+      horizontal += Number(command[1]);
+    } else if (command[0] === "down") {
+      depth += Number(command[1]);
+    } else if (command[0] === "up") {
+      depth -= Number(command[1]);
+    }
+  }
+  return horizontal * depth;
+};
+
+console.log(answer());
